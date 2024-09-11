5b3cfe3d78ef6c19609ca32aa7aef44c4de78afb
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2024-09-10 19:59:23 -0700

    display chart

--- hello/main.py
+++ hello/main.py
@@ -9,10 +9,30 @@ from writer.core import WriterState
 
 # STATE INIT
 
+from data import get_data
+
+data = get_data()
+
+# vegalite spec
+spec = {
+    "mark": "bar",
+    "data": {"values": data.reset_index().to_dict(orient="records")},
+    "encoding": {
+        "x": {
+            "field": "index",
+            "type": "temporal",
+        },
+        "y": {
+            "field": "count",
+            "type": "quantitative",
+        },
+    },
+}
+
 
 initial_state = wf.init_state(
     {
         "my_text": "Hello, world!!!",
-        "my_chart": "chart",
+        "my_chart": spec,
     }
 )
